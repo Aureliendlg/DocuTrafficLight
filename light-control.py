c79b05b323ba67eab3098dbebba39317232f9266
Aureliendlg <Aureliendlg@users.noreply.github.com>
2016-03-12 16:45:15 +0000

    Update light-control.py

--- light-control.py
+++ light-control.py
@@ -10,7 +10,7 @@ if os.getuid() != 0:
 
 parser = argparse.ArgumentParser(description = 'Control Traffic Signal State')
 parser.add_argument('color', choices = ['RED', 'YLLW', 'GRN'], help = 'Color of light')
-parser.add_argument('state', choices = ['on', 'off', 'blink'], help = 'State of light')
+parser.add_argument('state', choices = [1, 0, 'blink'], help = 'State of light')
 parser.add_argument('--verbose', '-v', action = 'store_true')
 args = parser.parse_args()
 
@@ -41,4 +41,4 @@ GPIO.setwarnings(False)
 
 # Trigger the GPIO output
 GPIO.setup(pin, GPIO.OUT)
-GPIO.output(pin, 1)
+GPIO.output(pin, state)
